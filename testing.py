809ec1f6290c6110ffde8e1ccfe4a212952ff604
xXA2005 <A2005BALILA@gmail.com>
2023-08-19 18:59:37 +0300

    stuff

---
+++ testing.py
@@ -0,0 +1,33 @@
+from keras.models import load_model
+from PIL import Image
+import numpy as np
+from tensorflow.keras.preprocessing.image import img_to_array
+import io
+import time
+import os
+
+np.set_printoptions(suppress=True)
+model = load_model('model.h5')
+
+
+class_names = ["dog","other","tree"]
+
+
+
+def predict(path):
+    with open(path,"rb") as f:
+        imageb =  f.read()
+    img = img_to_array(Image.open(io.BytesIO(imageb)))
+    img = np.expand_dims(img, axis=0)
+    prediction = model.predict(img)
+    return class_names[np.argmax(prediction, axis=1)[0]]
+
+
+
+
+if __name__ == '__main__':
+    start = time.time()
+    l = len(os.listdir('testing'))
+    for img in os.listdir('testing'):
+        print(f'{img} is {predict(f"./testing/{img}")}')
+    print(f"predicted {l} images in {time.time()-start}")
